7a8370647e2e7533e185c52b408f66596ed30067
Federico Esteban <federico.esteban@wolox.com.ar>
2018-10-09 21:38:43 -0300

    First steps for navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
+import true_logo from './true.svg';
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    sessionId: 0
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact={true} path="/" render={() => (
+            <header className="App-header"> 
+              <img src={logo} alt="logo" />
+              <img src={true_logo} alt="logo" />
+              <div className="component-wrapper">
+                <input type="text" value={this.state.sessionId} />
+                <Link to='/start'>
+                  <button className="button">
+                    Search
+                  </button>
+                </Link>
+              </div>
+            </header>
+          )}/>
+          <div>
+            <Route path="/start" component={HelloWorld}/>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
