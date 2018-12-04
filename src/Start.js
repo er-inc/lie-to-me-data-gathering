dc2a78ab5ea12ca330178ca18b1d47e4bc83f309
Daniela P. Riesgo <danielap.riesgo@gmail.com>
2018-12-04 17:41:07 -0300

    Adding loading and updating UI

--- src/Start.js
+++ src/Start.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
 
 class Start extends React.Component {
   constructor() {
     super();
     this.state = {
       conditionsAccepted: false,
-      sessionId: '',
+      sessionCode: '',
+      email: '',
+      requestInProgress: false,
       requestFulfilled: false,
+      sessionId: null,
       token: null,
       apiKey: null
     }
   }
 
-  updateInputvalue(evt) {
+  updateSessionInputValue(session) {
     this.setState({
-      sessionId: evt.target.value
+      sessionCode: session
+    });
+  }
+
+  updateEmailInputValue(email) {
+    this.setState({
+      email: email
     });
   }
 
@@ -27,20 +37,23 @@ class Start extends React.Component {
   }
 
   makeStartRequests() {
+    this.setState({requestInProgress: true})
       //TODO: Replace with correct URL
     axios.get('https://api.github.com/users/danielaRiesgo')
       .then(response => this.setState({
           //username: response.data.name,
+          requestInProgress: false,
           requestFulfilled: true,
+          sessionId: '2_MX40NjE3ODUwMn5-MTU0Mzk1MjA4MjQ0Mn5Gb2xwYSt1L3NvOUNkUHZ0UDhYanE5cmZ-fg',
           apiKey: '46178502',
-          token: 'T1==cGFydG5lcl9pZD00NjE3ODUwMiZzaWc9MDI4NmE5MzE4YzY1MWZiYTlkMDU5MjcwYWJjMjk1MWIzODBmMTE4MTpzZXNzaW9uX2lkPTJfTVg0ME5qRTNPRFV3TW41LU1UVXpPVEV6TmpVME5qa3pNSDVNZWtGWEwzRkhXakJOUmtKVFYwcHRSR2xPUVUxbWFuWi1mZyZjcmVhdGVfdGltZT0xNTM5MTM2NTYyJm5vbmNlPTAuMTYyODk4NDU5MTM2NzkwOCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTM5NzQxMzY0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9'
+          token: 'T1==cGFydG5lcl9pZD00NjE3ODUwMiZzaWc9MzFlMDlkYWNiNDljZWFiMTE4MjMzYTBiMGU0ZDVkYzU4ODQ3YWM2YjpzZXNzaW9uX2lkPTJfTVg0ME5qRTNPRFV3TW41LU1UVTBNemsxTWpBNE1qUTBNbjVHYjJ4d1lTdDFMM052T1VOa1VIWjBVRGhZYW5FNWNtWi1mZyZjcmVhdGVfdGltZT0xNTQzOTUyMDk1Jm5vbmNlPTAuNTcyMjQ3OTIwNTk5OTQ3MSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTQ2NTQ0MDk0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9'
       }))
   }
 
   render() {
     const location = {
-      pathname: `/session/${this.state.sessionId}/instructions`,
-      state: { token: this.state.token, apiKey: this.state.apiKey }
+      pathname: `/session/${this.state.sessionCode}/instructions`,
+      state: { token: this.state.token, apiKey: this.state.apiKey, sessionId: this.state.sessionId }
     }
     if (this.state.requestFulfilled) {
       return <Redirect to={ location } />;
@@ -68,13 +81,28 @@ class Start extends React.Component {
         { this.state.conditionsAccepted && (
           <div className="component-wrapper">
             <h2>¡Gracias por participar!</h2>
-            <p>Ingresá el código que te enviamos para esta sesión:</p>
-            <input type="text" value={this.state.sessionId} placeholder={"Id de Sesión"} onChange={evt => this.updateInputvalue(evt)} />
-            <button className="button" onClick={() => this.makeStartRequests()}>
+            <p>Ingresá el código que te enviamos para esta sesión y tu email:</p>
+            <input type="text"
+                value={this.state.sessionCode}
+                placeholder={"Código de Sesión"}
+                onChange={evt => this.updateSessionInputValue(evt.target.value)}
+                width={100} />
+            <input type="text"
+                value={this.state.email}
+                placeholder={"Email"}
+                onChange={evt => this.updateEmailInputValue(evt.target.value)}
+                width={200} />
+            <button className="button"
+                disabled = {!this.state.email || !this.state.sessionCode}
+                onClick={() => this.makeStartRequests()}>
             Ir
             </button>
           </div>
         )}
+        <br/>
+        { this.state.requestInProgress &&
+          <ReactLoading className="loader" type="spinningBubbles" color="#000000" />
+        }
       </div>
     )
   }
